497639338d6a517edfbf82a8ccd35e206d62bcc0
TejasdeepxSingh <tejas.singh_ug25@ashoka.edu.in>
2024-10-19 18:45:32 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,24 @@
-#include "HashTable.cpp"
-#include <iostream>
-
 int main() {
     int initialSize = 7; 
     HashTable ht(initialSize);
 
-    // Example test cases
+    // Example test case
     ht.insert(1);
     ht.printTable();
-    
     ht.insert(6);
     ht.printTable();
-    
     ht.insert(15);
-    ht.printTable();
-    
+    ht.printTable(); 
     ht.insert(25);
     ht.printTable();
-    
     ht.remove(15);
     ht.printTable();
-    
     ht.insert(29);  
     ht.printTable(); 
 
-    int find = ht.search(25);
-    if (find != -1) {
-        std::cout << "Element found at index: " << find << std::endl;
-    } else {
-        std::cout << "Element not found" << std::endl;
-    }
+    int find = ht.search(22);
+    std::cout << "Found at:" << find << std::endl;
 
     return 0;
 }
+
